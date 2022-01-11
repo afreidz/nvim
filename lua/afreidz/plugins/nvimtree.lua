a63504d874afe82793bcd43a98e9dc240bcab8bd
afreidz <afreidenfelds@gmail.com>
2022-01-11 12:37:36 -0600

    re-enable tree icons

--- lua/afreidz/plugins/nvimtree.lua
+++ lua/afreidz/plugins/nvimtree.lua
@@ -23,7 +23,7 @@ g.nvim_tree_icons = icons
 g.nvim_tree_indent_markers = 1
 g.nvim_tree_disable_window_picker = 1
 g.nvim_tree_highlight_opened_files = 1
-g.nvim_tree_show_icons = { git = 1, folder_arrows = 0 }
+g.nvim_tree_show_icons = { git = 1, folder_arrows = 0, folders = 1, files = 1 }
 
 local present, tree = pcall(require, "nvim-tree")
 if not present then return end
