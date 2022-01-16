754ee7a2e06d5930a74a28f73287bb3d0163073f
afreidz <afreidenfelds@gmail.com>
2022-01-16 13:47:34 -0600

    tone down icon colors

---
+++ lua/afreidz/plugins/icons.lua
@@ -0,0 +1,13 @@
+local present, icons = pcall(require, 'nvim-web-devicons')
+if not present then return end
+
+local colors = require('afcolors.api.colors').get_colors()
+local current = icons.get_icons()
+local new = {}
+
+for key, icon in pairs(current) do
+    icon.color = colors.gray0
+    new[key] = icon
+end
+
+icons.set_icon(new)
