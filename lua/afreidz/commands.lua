0d18b2ec20ba82a2ed6953497d114f2fdaebd286
afreidz <afreidenfelds@gmail.com>
2022-01-24 11:24:08 -0600

    open file tree on session load

--- lua/afreidz/commands.lua
+++ lua/afreidz/commands.lua
@@ -1,5 +1,3 @@
 local cmd = vim.cmd
 cmd ('colorscheme afcolors')
-
-local colors = require('afcolors.api.colors').get_colors();
-cmd('hi BufferLineFill guibg='..colors.black3)
+cmd('au SessionLoadPost * NvimTreeOpen')
