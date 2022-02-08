4804642d68baab7666d1f7dcce120970e7631252
afreidz <afreidenfelds@gmail.com>
2022-02-08 16:26:11 -0600

    adds svelte as a prettier filetype

--- lua/afreidz/plugins/lsp.lua
+++ lua/afreidz/plugins/lsp.lua
@@ -65,7 +65,9 @@ lspconfig.tsserver.setup({
 null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.formatting.prettier.with({
+			extra_filetypes = { "svelte" },
+		}),
 		null_ls.builtins.diagnostics.eslint_d,
 		null_ls.builtins.code_actions.eslint_d,
 	},
