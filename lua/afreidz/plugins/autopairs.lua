f6441e2e31603833794790527cfab082d366db4d
afreidz <afreidenfelds@gmail.com>
2022-02-09 20:33:00 -0600

    remove fast wrap from autopairs

--- lua/afreidz/plugins/autopairs.lua
+++ lua/afreidz/plugins/autopairs.lua
@@ -11,17 +11,6 @@ npairs.setup({
 		javascript = { "string", "template_string" },
 	},
 	disable_filetype = { "TelescopePrompt" },
-	fast_wrap = {
-		map = "<M-e>",
-		chars = { "{", "[", "(", '"', "'", "<" },
-		pattern = string.gsub([[ [%'%"%)%>%]%)%}%,%>] ]], "%s+", ""),
-		offset = 0,
-		end_key = "$",
-		keys = "qwertyuiopzxcvbnmasdfghjkl",
-		check_comma = true,
-		highlight = "PmenuSel",
-		highlight_grey = "LineNr",
-	},
 })
 
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
